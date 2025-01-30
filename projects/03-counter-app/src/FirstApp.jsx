d4418914431269e2a7c34cb555db993c8b54745c
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2025-01-29 18:38:39 -0600

    Added last changes for section 4

--- projects/03-counter-app/src/FirstApp.jsx
+++ projects/03-counter-app/src/FirstApp.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 // const getResult = () => {
 //   return 'Hola Mundo!!'
 // }
@@ -7,15 +9,26 @@
 //   title: 'Mauricio'
 // };
 
-const FirstApp = ({title}) => {
+const FirstApp = ({ 
+  title = 'No hay titulo',
+  subTitle = 'No hay subtitulo',
+  name='Mauricio Covarrubias'
+}) => {
   // console.log(props);
   return (
     <>
       <h1>{ title }</h1>
       {/* <code>{ JSON.stringify(newMessage) }</code> */}
-      <p>Soy un subtitulo</p>
+      <p>{ subTitle }</p>
+      <p>{ name }</p>
     </>
   );
+}
+
+FirstApp.propTypes = {
+  name: PropTypes.string,
+  subTitle: PropTypes.string,
+  title: PropTypes.string,
 }
 
 export {
--- projects/03-counter-app/src/main.jsx
+++ projects/03-counter-app/src/main.jsx
@@ -1,12 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
+// import FirstApp from './FirstApp';
 // import HelloWorldApp from './HelloWorldApp';
-import FirstApp from './FirstApp';
-import './styles.css'
+import CounterApp from './CounterApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp title="Hola Mundo"/>
+    {/* <FirstApp subTitle='Soy un subtitulo'/> */}
+    <CounterApp value={0} />
   </StrictMode>
-)
+);
